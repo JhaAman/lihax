5f1fd9891545e7704fa1431a8f9fbf8ea408c3e7
YomanAwe <gunujha@gmail.com>
2016-08-09 10:18:05 -0400

    gedit
    boi

---
+++ Aggressive Simulator/Assets/My Work/Scripts/CarMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarMovement : MonoBehaviour {
+
+    int thrust = 0;
+    int speed = 0;
+    int acceleration = 0;
+
+	
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        
+    }
+
+}
