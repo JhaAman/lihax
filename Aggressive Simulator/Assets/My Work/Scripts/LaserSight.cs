b26f5635c4aa3116de3ca8e00b6ea9b22843a29f
Jacob Nazarenko <jnazaren@gmail.com>
2016-08-28 02:56:20 -0400

    Finished code for analyzing different variables, which has most of the sockets in place; NEEDS TESTING!!

--- Aggressive Simulator/Assets/My Work/Scripts/LaserSight.cs
+++ Aggressive Simulator/Assets/My Work/Scripts/LaserSight.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
 [RequireComponent (typeof (LineRenderer))]
 public class LaserSight : MonoBehaviour {
@@ -18,23 +19,41 @@ public class LaserSight : MonoBehaviour {
 	private LineRenderer lr;
 
 	public float totalAngleDesired = 270f;
-	public float angleIncrement = .5f;
+	public float angleIncrement = .25f;
 
 	public Vector2[] distances;
 	int totalAngleIncremented; 
 
 	int frameNumber = 0;
 
-	Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+	public Socket dataSenderOne;
+	public IPAddress hostIP;
+	public IPEndPoint epOne;
+	public Socket dataSenderTwo;
+	public IPEndPoint epTwo;
+
 
 	void Start () {
 		lr = GetComponent<LineRenderer>();
-		SwitchToTurnRight();
 
 		totalAngleIncremented = (int) (totalAngleDesired/ angleIncrement); //i.e: 270 into half angles - 270/.5 = 540
 		distances = new Vector2[totalAngleIncremented];
 
+		dataSenderOne = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+		dataSenderTwo = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
 
+		IPAddress[] ipv4Addresses = Array.FindAll(
+			Dns.GetHostEntry(Dns.GetHostName()).AddressList,
+			a => a.AddressFamily == AddressFamily.InterNetwork);
+		hostIP = ipv4Addresses [0];
+
+		epOne = new IPEndPoint(hostIP, 5510);
+		print ("Opening a udp socket at: " + epOne);
+		dataSenderOne.Connect(epOne); 
+
+		epTwo = new IPEndPoint(hostIP, 5520);
+		print ("Opening a udp socket at: " + epTwo);
+		dataSenderTwo.Connect(epTwo); 
 	}
 
 	void Update () {
@@ -59,6 +78,7 @@ public class LaserSight : MonoBehaviour {
 		pointOfOrigin.localEulerAngles = initialFrameAngleVector;
 
 		using (StreamWriter sw = new StreamWriter (fileName)) {
+			byte[] msg = new byte[4320];
 			for (int i = 0; i < totalAngleIncremented; i++) {
 				//send a raycast at current angle
 				if (Physics.Raycast (pointOfOrigin.position, pointOfOrigin.forward, out Hit)) {
@@ -68,6 +88,10 @@ public class LaserSight : MonoBehaviour {
 					sw.Write (distances [i].x);
 					sw.Write (",");
 					sw.WriteLine (distances [i].y);
+					msg [4*i] = BitConverter.GetBytes(distances[i].y) [0];
+					msg [(4*i)+1] = BitConverter.GetBytes(distances[i].y) [1];
+					msg [(4*i)+2] = BitConverter.GetBytes(distances[i].y) [2];
+					msg [(4*i)+3] = BitConverter.GetBytes(distances[i].y) [3];
 				} else {
 					Debug.LogError ("There isn't an object in front of me. What is going on.");
 				}
@@ -78,22 +102,25 @@ public class LaserSight : MonoBehaviour {
 				settingAngleVector.y = currentAngle;
 				pointOfOrigin.localEulerAngles = settingAngleVector;
 			}
+
+			dataSenderOne.Send (msg);
+			dataSenderTwo.Send (msg);
 				
 		}
 
 	}
 
-	void OnDisable() {
+	void OnApplicationQuit() {
 		var fileName = "Frame.txt";
 
 		using (StreamWriter sw = new StreamWriter (fileName)) {
 			sw.WriteLine ("END");
 		}
 
-	}
+		String stop = "STOP";
+		byte[] msg = Encoding.ASCII.GetBytes (stop);
+		dataSenderTwo.Send (msg);
 
-	void SwitchToTurnRight() {
-		//start collecting lidar data
 	}
 
 }
